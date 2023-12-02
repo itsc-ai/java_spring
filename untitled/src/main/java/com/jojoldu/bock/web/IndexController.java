28f2ced23aa51aef6e92aa7f465944cbd670c29f
DESKTOP-LUCTNC5\user <dhgudehd979@gmail.com>
2023-12-02 23:48:40 +0900

    구글 로그인 연동 완료

--- untitled/src/main/java/com/jojoldu/bock/web/IndexController.java
+++ untitled/src/main/java/com/jojoldu/bock/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jojoldu.bock.web;
 
+import com.fasterxml.jackson.databind.DatabindContext;
+import com.jojoldu.bock.config.auth.dto.SessionUser;
 import com.jojoldu.bock.service.PostsService;
 import com.jojoldu.bock.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
