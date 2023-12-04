7e17987139e5601dcfaa4dce8586935656d5dc15
DESKTOP-LUCTNC5\user <dhgudehd979@gmail.com>
2023-12-04 15:24:31 +0900

    네이버 로그인 추가 및 스프링 시큐리티 설정

--- untitled/src/main/java/com/jojoldu/bock/web/IndexController.java
+++ untitled/src/main/java/com/jojoldu/bock/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.jojoldu.bock.web;
 
 import com.fasterxml.jackson.databind.DatabindContext;
+import com.jojoldu.bock.config.auth.LoginUser;
 import com.jojoldu.bock.config.auth.dto.SessionUser;
 import com.jojoldu.bock.service.PostsService;
 import com.jojoldu.bock.web.dto.PostsResponseDto;
@@ -20,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
--- untitled/src/test/java/com/jojoldu/bock/web/PostsApiControllerTest.java
+++ untitled/src/test/java/com/jojoldu/bock/web/PostsApiControllerTest.java
@@ -1,24 +1,35 @@
 package com.jojoldu.bock.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jojoldu.bock.domain.posts.Posts;
 import com.jojoldu.bock.domain.posts.PostsRepository;
 import com.jojoldu.bock.web.dto.PostsSaveRequestDto;
 import com.jojoldu.bock.web.dto.PostsUpdateRequestDto;
+
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
 
@@ -36,7 +47,21 @@ public class PostsApiControllerTest {
         postsRepository.deleteAll();
     }
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
+    @WithMockUser(roles="USER")
     void Posts_등록된다() throws Exception {
 
         //given
@@ -53,9 +78,14 @@ public class PostsApiControllerTest {
         //when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo((HttpStatus.OK));
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//        assertThat(responseEntity.getStatusCode()).isEqualTo((HttpStatus.OK));
+//        assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> postsList = postsRepository.findAll();
         assertThat(postsList.get(0).getTitle()).isEqualTo(title);
@@ -64,6 +94,8 @@ public class PostsApiControllerTest {
     }
 
     //given
+    @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정() throws Exception {
         //given
         Posts savedPosts = postsRepository.save(Posts.builder()
@@ -87,9 +119,13 @@ public class PostsApiControllerTest {
     //when
     ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
 
+    mvc.perform(put(url)
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(new ObjectMapper().writeValueAsString(requestDto)))
+            .andExpect(status().isOk());
     //then
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//    assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
     List<Posts> all = postsRepository.findAll();
     assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
